14f687911a759de1d261262434a5781cc4612ebf
LLath <rothpatrick12@yahoo.de>
2022-11-05 16:00:34 +0100

    feat: / page

---
+++ src/views/MainView.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+<template>
+  <div>
+    <RouterLink
+      v-for="route in this.$router.options.routes"
+      :key="route.path"
+      :to="route.path"
+      >{{ route.name }}</RouterLink
+    >
+  </div>
+</template>
+
+<style scoped>
+div {
+  display: flex;
+}
+a {
+  display: block;
+  text-align: center;
+  padding: 1rem;
+  margin: 1rem;
+  background-color: grey;
+  color: white;
+  text-decoration: none;
+  width: 25%;
+}
+</style>
