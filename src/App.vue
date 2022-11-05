aa67b7f9657fd5395880120736247a31f5805455
LLath <rothpatrick12@yahoo.de>
2022-11-05 15:58:11 +0100

    chore: clear setup

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,26 @@ import { RouterView } from "vue-router";
 </template>
 
 <style scoped>
+/* nunito-sans-regular - latin */
+@font-face {
+  font-family: "Nunito Sans";
+  font-style: normal;
+  font-weight: 400;
+  src: url("./assets/fonts/nunito-sans-v12-latin-regular.eot"); /* IE9 Compat Modes */
+  src: local(""),
+    url("./assets/fonts/nunito-sans-v12-latin-regular.eot?#iefix")
+      format("embedded-opentype"),
+    /* IE6-IE8 */ url("./assets/fonts/nunito-sans-v12-latin-regular.woff2")
+      format("woff2"),
+    /* Super Modern Browsers */
+      url("./assets/fonts/nunito-sans-v12-latin-regular.woff") format("woff"),
+    /* Modern Browsers */
+      url("./assets/fonts/nunito-sans-v12-latin-regular.ttf") format("truetype"),
+    /* Safari, Android, iOS */
+      url("./assets/fonts/nunito-sans-v12-latin-regular.svg#NunitoSans")
+      format("svg"); /* Legacy iOS */
+}
+
 body {
   height: 100vh;
   width: 100vh;
@@ -15,5 +35,6 @@ main {
   height: 100%;
   width: 100%;
   justify-content: center;
+  font-family: "Nunito Sans";
 }
 </style>
