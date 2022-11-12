8badf59be41a42cad62c9f0c1a5d823a1d31a707
LLath <rothpatrick12@yahoo.de>
2022-11-12 11:57:26 +0100

    chore: setup team icons

---
+++ src/constants/owTeams.js
@@ -0,0 +1,6 @@
+export const TEAMS = {
+  "Biscuits & Gravy": { icon: "teamIcons/Biscuits_And_Gravy.jpg" },
+  "High Noot": { icon: "teamIcons/High_Noot.jpg" },
+  "High Tide": { icon: "teamIcons/High_Tide.jpg" },
+  Justice4Mei: { icon: "teamIcons/Justice_4_Mei_Small.jpg" },
+};
